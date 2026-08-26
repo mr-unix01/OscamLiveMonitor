27760f6ca4c9c299f1314624739eb74de06ff357
mr-unix01 <167810150+mr-unix01@users.noreply.github.com>
2026-08-26 20:03:57 +0200

    Imposta versione 0.9.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mrunix.oscamlivemonitor"
         minSdk = 26
         targetSdk = 37
-        versionCode = 912
-        versionName = "0.9.12"
+        versionCode = 913
+        versionName = "0.9.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
